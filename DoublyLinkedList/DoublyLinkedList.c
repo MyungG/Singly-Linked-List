904964a077311ed98d0a30ca4eb182414ae32cc9
MyungG <64455972+MyungG@users.noreply.github.com>
2020-08-26 20:49:40 +0900

    Add files via upload

---
+++ DoublyLinkedList/DoublyLinkedList.c
@@ -0,0 +1,127 @@
+#include "DoublyLinkedList.h"
+
+//노드 생성
+Node* DLL_CreateNode(ElementType Data)
+{
+	Node* NewNode = (Node*)malloc(sizeof(Node));
+
+	NewNode->Data = Data;
+	NewNode->PrevNode = NULL;
+	NewNode->NextNode = NULL;
+
+	return NewNode;
+}
+
+//노드 소멸
+void DLL_DestroyNode(Node* Node)
+{
+	free(Node);
+}
+
+//노드 추가
+void DLL_AppendNode(Node** Head, Node* NewNode)
+{
+	if ((*Head) == NULL)
+	{
+		*Head = NewNode;
+	}
+	else
+	{
+		Node* Tail = (*Head);
+		while (Tail->NextNode != NULL)
+		{
+			Tail = Tail->NextNode;
+		}
+		Tail->NextNode = NewNode;
+		NewNode->PrevNode = Tail;
+	}
+}
+
+//노드 삽입
+void DLL_InsertAfter(Node* Current, Node* NewNode)
+{
+	NewNode->NextNode = Current->NextNode;
+	NewNode->PrevNode = Current;
+
+	if (Current->NextNode != NULL)
+	{
+		Current->NextNode->PrevNode = NewNode;
+	}
+	Current->NextNode = NewNode;
+}
+
+//노드 제거
+void DLL_RemoveNode(Node** Head, Node* Remove)
+{
+	if ((*Head) == Remove)
+	{
+		(*Head) = Remove->NextNode;
+		if ((*Head) != NULL)
+		{
+			(*Head)->PrevNode = NULL;
+		}
+		
+		Remove->PrevNode = NULL;
+		Remove->NextNode = NULL;
+	}
+	else
+	{
+		Node* Temp = Remove;
+
+		if (Remove->PrevNode != NULL)
+		{
+			Remove->PrevNode->NextNode = Temp->NextNode;
+		}
+		if (Remove->NextNode != NULL)
+		{
+			Remove->NextNode->PrevNode = Temp->PrevNode;
+		}
+
+		Remove->PrevNode = NULL;
+		Remove->NextNode = NULL;
+
+	}
+}
+
+Node* DLL_GetNodeAt(Node* Head, int Location)
+{
+	Node* Current = Head;
+
+	while (Current != NULL && (--Location) >= 0)
+	{
+		Current = Current->NextNode;
+	}
+
+	return Current;
+}
+
+int DLL_GetNodeCount(Node* Head)
+{
+	int count = 0;
+	Node* Current = Head;
+
+	while (Current != NULL)
+	{
+		Current = Current->NextNode;
+		count++;
+	}
+	return count;
+}
+
+void PrintReverse(Node* Head)
+{
+	Node* Current = Head;
+	int i;
+	int count = 0;
+
+	count = DLL_GetNodeCount(Head);
+
+
+	for (i = count-1; i >= 0; i--)
+	{
+		Current = DLL_GetNodeAt(Current, i);
+		printf("List[%d] : %d\n", i, Current->Data);
+	}
+
+
+}
